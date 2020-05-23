1b52b3cab3f7c86749bdec6d926189b55778d65f
sudhaKantamneni <65807648+sudhaKantamneni@users.noreply.github.com>
2020-05-23 22:03:33 +0530

    Update InputFactory.java

--- DatingRecommendations-Sudha/src/factory/InputFactory.java
+++ DatingRecommendations-Sudha/src/factory/InputFactory.java
@@ -4,15 +4,17 @@ import model.InputType;
 import service.FileInput;
 import service.InputService;
 
-/*Factory class to get the input.txt
-*/
+/**
+* Factory class to get the input.txt
+ * @author admin
+ */
 public class InputFactory {
 
     public static InputService getInputFrom(InputType inputType) {
         switch (inputType) {
-                //change the local file path
-            case FILE: return new FileInput("C:\\Users\\admin\\git\\DatingRecommendations-Sudha\\DatingRecommendations-Sudha\\src\\input.txt");
-            default: return new FileInput("C:\\Users\\admin\\git\\DatingRecommendations-Sudha\\DatingRecommendations-Sudha\\src\\input.txt");
+                //input Filefile path
+               case FILE: return new FileInput(".\\DatingRecommendations-Sudha\\src\\input.txt");
+               default: return new FileInput(".\\DatingRecommendations-Sudha\\src\\input.txt");
         }
     }
 }
